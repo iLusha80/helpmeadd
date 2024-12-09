129443c57f9e9903c27e44eaa63e286b0d8dc1bc
iLusha80 <rastyfx@gmail.com>
2024-12-09 23:17:56 +0300

    формирование списка данных и добавление единой вставкой так же проверка на тайм без дейстействий в таймлайне

--- parsers/parser_match_time_line.py
+++ parsers/parser_match_time_line.py
@@ -16,7 +16,6 @@ css_selectors = {
     'incident'      : '[class$="incident"]',
     'timebox'       : '[class*="timeBox"]',
     'event_type'    : 'svg'
-
 }
 
 
@@ -35,21 +34,16 @@ class ParserMatchTimeLine:
 
         Utils.get_flashscore_url(driver=driver, url=url)
 
-        result = Utils.load_page(driver=driver, css_selector=css_selectors['time_line_div'])
-
-        if result != True:
-            logger.error(f"Err - {result}\nurl - {url}")
-            return False
-        else:
-            logger.debug(f"Success- {result}!\n Вот это дело! потекли данные")
-
         divs = driver.find_elements(By.CSS_SELECTOR, css_selectors['time_line_div'])
 
+        result = list()
         for div in divs:
             css_class = div.get_attribute('class')
             if 'section__title' in css_class:
                 half = div.find_element(By.CSS_SELECTOR, css_selectors['half_title']).text
                 score = div.find_element(By.CSS_SELECTOR, css_selectors['half_score']).text
+            elif 'smv__empty' in css_class:
+                continue
             else:
                 if 'away' in css_class:
                     team = 2
@@ -60,23 +54,19 @@ class ParserMatchTimeLine:
                     data['match_id'] = match_id
                     data['team_type'] = team
                     data['half'] = half
+                    result.append(data)
+        MatchTimeLineData.insert_many(db=db, data=result)
 
-                    MatchTimeLineData.insert(db, **data)
 
 
     @staticmethod
     def get_data_from_timeline_row(div):
         data = dict()
 
-        incedent = div.find_element(By.CSS_SELECTOR, css_selectors['incident'])
+        incident = div.find_element(By.CSS_SELECTOR, css_selectors['incident'])
+        # тут можно будет вернуть трай эксепт для отлова пустых таймов и пропуска их
 
-        # try:
-        #     incedent = div.find_element(By.CSS_SELECTOR, css_selectors['incident'])
-        # except:
-        #     logger.error(f"Не найдено инцидента в css_selectors['incident']: {css_selectors['incident']}")
-        #     return None
-
-        timebox = incedent.find_element(By.CSS_SELECTOR, css_selectors['timebox']).text
+        timebox = incident.find_element(By.CSS_SELECTOR, css_selectors['timebox']).text
 
         try:
             tmp = timebox.split("+")[0].replace("'", "")
@@ -91,15 +81,15 @@ class ParserMatchTimeLine:
         except:
             data['add_minutes'] = 0
 
-        data['event_type'] = incedent.find_element(By.CSS_SELECTOR, css_selectors['event_type']).get_attribute('class')
+        data['event_type'] = incident.find_element(By.CSS_SELECTOR, css_selectors['event_type']).get_attribute('class')
 
         if not data['event_type']:
             data['event_type'] = '?gol?'
 
-        data['player_name'] = incedent.find_elements(By.CSS_SELECTOR, 'a')[0].text
+        data['player_name'] = incident.find_elements(By.CSS_SELECTOR, 'a')[0].text
 
-        if len(incedent.find_elements(By.CSS_SELECTOR, 'a')) > 1:
-            data['assist_player_name'] = incedent.find_elements(By.CSS_SELECTOR, 'a')[1].text
+        if len(incident.find_elements(By.CSS_SELECTOR, 'a')) > 1:
+            data['assist_player_name'] = incident.find_elements(By.CSS_SELECTOR, 'a')[1].text
         else:
             data['assist_player_name'] = 'Не указано'
         return data
